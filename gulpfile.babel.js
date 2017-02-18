23c1acb13f902a5c6f1c24325c0e5a37ccde9ca4
dashukin <dashukin@gmail.com>
2017-02-18 15:25:39 +0300

    Add gulp task for version updates in manifest.hjson and package.json files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,22 +11,37 @@
  * @property UglifyJsPlugin
  */
 
+/**
+ * @name fsExtra
+ * @type {Object}
+ * @property readFileSync
+ * @property writeFileSync
+ */
+
 import gulp from 'gulp';
 import del from 'del';
 import sass from 'gulp-sass';
 import webpack from 'webpack';
 import gulpWebpack from 'gulp-webpack';
 import webpackConfig from './webpack.config';
-import hjson from 'gulp-hjson';
+import gulpHjson from 'gulp-hjson';
+import hjson from 'hjson';
 import zip from 'gulp-zip';
 import runSequence from 'run-sequence';
+import yargs from 'yargs';
+import fsExtra from 'fs-extra';
+
+const {argv} = yargs;
+
+const encoding = 'utf8';
 
 // src paths
+const packageSrcPath = './package.json';
 const appSrcPath = './src/app';
 const jsSrcPath = `${appSrcPath}/js/**/*.js`;
 const scssSrcPath = `${appSrcPath}/scss/**/*.scss`;
-const hjsonSrcPath = `${appSrcPath}/*.hjson`;
-const staticFilesSrcFiles = [`${appSrcPath}/**/*`, `!${jsSrcPath}`, `!${scssSrcPath}`, `!${hjsonSrcPath}`];
+const manifestSrcPath = `${appSrcPath}/manifest.hjson`;
+const staticFilesSrcFiles = [`${appSrcPath}/**/*`, `!${jsSrcPath}`, `!${scssSrcPath}`, `!${manifestSrcPath}`];
 
 // build paths
 const buildPath = './build';
@@ -35,19 +50,26 @@ const jsBuildPath = `${appBuildPath}/js`;
 const cssBuildPath = `${appBuildPath}/css`;
 const zipSrcPath = `${appBuildPath}/**/*`;
 
+const hjsonConfig = {
+	keepWsc: true,
+	bracesSameLine: true,
+	quotes: 'all',
+	space: '	'
+};
+
 
 gulp.task('clean', () => {
 	return del(`${buildPath}/*`);
 });
 
 gulp.task('build-manifest', () => {
-	return gulp.src(hjsonSrcPath)
-		.pipe(hjson({to: 'json', jsonSpace: ' ' }))
+	return gulp.src(manifestSrcPath)
+		.pipe(gulpHjson({to: 'json', jsonSpace: ' '}))
 		.pipe(gulp.dest(appBuildPath));
 });
 
 gulp.task('watch-manifest', () => {
-	return gulp.watch(hjsonSrcPath, ['build-manifest']);
+	return gulp.watch(manifestSrcPath, ['build-manifest']);
 });
 
 gulp.task('build-scss', () => {
@@ -112,6 +134,53 @@ gulp.task('build-static', () => {
 
 gulp.task('watch-static', () => {
 	return gulp.watch(staticFilesSrcFiles, ['build-static'])
+});
+
+gulp.task('version', () => {
+	let packageData = require(packageSrcPath);
+	let {version} = packageData;
+	let [major = 0, minor = 0, patch = 0] = version.split('.').map(version => {
+		return +version;
+	});
+
+	let {patch: createPatch, minor: createMinor, major: createMajor} = argv;
+
+	if (createPatch) {
+		patch += 1;
+	} else if (createMinor) {
+		minor += 1;
+		patch = 0;
+	} else if (createMajor) {
+		major += 1;
+		minor = 0;
+		patch = 0;
+	} else {
+		console.log('Missing action type for version update');
+		return;
+	}
+
+	let newVersion = [major, minor, patch].join('.');
+
+	packageData.version = newVersion;
+
+	fsExtra.writeFileSync(packageSrcPath, JSON.stringify(packageData, null, 4), encoding);
+
+	let manifestText = fsExtra.readFileSync(manifestSrcPath, encoding);
+
+	let manifestData = hjson.parse(manifestText, hjsonConfig);
+
+	manifestData.version = newVersion;
+
+	fsExtra.writeFileSync(manifestSrcPath, hjson.stringify(manifestData, hjsonConfig), encoding);
+
+	console.log(`Version has been updated from ${version} to ${newVersion}`);
+})
+
+gulp.task('update-version', () => {
+	runSequence(
+		'version',
+		'build-manifest'
+	);
 });
 
 gulp.task('start-dev', () => {
