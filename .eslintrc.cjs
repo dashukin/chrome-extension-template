ba5f608ee807a144200c5ed6bf06690d3cbcea28
Vasili Molakhau <dashukin@gmail.com>
2024-02-16 17:42:14 +0300

    Add ESLint, prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,62 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'standard-with-typescript',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'class-methods-use-this': 'warn',
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'warn',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    '@typescript-eslint/strict-boolean-expressions': 'warn',
+  },
+  settings: {
+    'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.mjs', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
